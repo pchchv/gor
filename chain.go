ad9848a73b5eb37e3036b37e0d12914c65fcc8ac
pchchv <ipchchv@gmail.com>
2023-02-01 18:59:14 +0300

    Implement a Chain type

---
+++ chain.go
@@ -0,0 +1,32 @@
+package gor
+
+import "net/http"
+
+// ChainHandler is a http.Handler with support for handler composition and execution.
+type ChainHandler struct {
+	Endpoint    http.Handler
+	chain       http.Handler
+	Middlewares Middlewares
+}
+
+// Chain returns a Middlewares type from a slice of middleware handlers.
+func Chain(middlewares ...func(http.Handler) http.Handler) Middlewares {
+	return Middlewares(middlewares)
+}
+
+// chain builds a http.Handler composed of an embedded middleware stack and
+// an endpoint handler in the order in which they are passed.
+func chain(middlewares []func(http.Handler) http.Handler, endpoint http.Handler) http.Handler {
+	// return early if there are no any middlewares for the chain
+	if len(middlewares) == 0 {
+		return endpoint
+	}
+
+	// wrap the end handler with the middleware chain
+	h := middlewares[len(middlewares)-1](endpoint)
+	for i := len(middlewares) - 2; i >= 0; i-- {
+		h = middlewares[i](h)
+	}
+
+	return h
+}
