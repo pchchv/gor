029bd88389602c7c636c8083b5e01e09673e9501
pchchv <ipchchv@gmail.com>
2023-02-03 10:43:13 +0300

    Implement a MuxBasic test

---
+++ mux_test.go
@@ -0,0 +1,251 @@
+package gor
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type ctxKey struct {
+	name string
+}
+
+func TestMuxBasic(t *testing.T) {
+	var count uint64
+	countermw := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			count++
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	usermw := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, ctxKey{"user"}, "peter")
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	exmw := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(r.Context(), ctxKey{"ex"}, "a")
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	logbuf := bytes.NewBufferString("")
+	logmsg := "logmw test"
+	logmw := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			logbuf.WriteString(logmsg)
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	cxindex := func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		user := ctx.Value(ctxKey{"user"}).(string)
+		w.WriteHeader(200)
+		_, err := w.Write([]byte(fmt.Sprintf("hi %s", user)))
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	ping := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		_, err := w.Write([]byte("."))
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	headPing := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Ping", "1")
+		w.WriteHeader(200)
+	}
+
+	createPing := func(w http.ResponseWriter, r *http.Request) {
+		// create ....
+		w.WriteHeader(201)
+	}
+
+	pingAll := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		_, err := w.Write([]byte("ping all"))
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	pingAll2 := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		_, err := w.Write([]byte("ping all2"))
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	pingOne := func(w http.ResponseWriter, r *http.Request) {
+		idParam := URLParam(r, "id")
+		w.WriteHeader(200)
+		_, err := w.Write([]byte(fmt.Sprintf("ping one id: %s", idParam)))
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	pingWoop := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		_, err := w.Write([]byte("woop." + URLParam(r, "iidd")))
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	catchAll := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		_, err := w.Write([]byte("catchall"))
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	m := NewRouter()
+	m.Use(countermw)
+	m.Use(usermw)
+	m.Use(exmw)
+	m.Use(logmw)
+	m.Get("/", cxindex)
+	m.Method("GET", "/ping", http.HandlerFunc(ping))
+	m.MethodFunc("GET", "/pingall", pingAll)
+	m.MethodFunc("get", "/ping/all", pingAll)
+	m.Get("/ping/all2", pingAll2)
+
+	m.Head("/ping", headPing)
+	m.Post("/ping", createPing)
+	m.Get("/ping/{id}", pingWoop)
+	m.Get("/ping/{id}", pingOne) // expected to overwrite to pingOne handler
+	m.Get("/ping/{iidd}/woop", pingWoop)
+	m.HandleFunc("/admin/*", catchAll)
+	// m.Post("/admin/*", catchAll)
+
+	ts := httptest.NewServer(m)
+	defer ts.Close()
+
+	// GET /
+	if _, body := testRequest(t, ts, "GET", "/", nil); body != "hi peter" {
+		t.Fatalf(body)
+	}
+	tlogmsg, _ := logbuf.ReadString(0)
+	if tlogmsg != logmsg {
+		t.Error("expecting log message from middleware:", logmsg)
+	}
+
+	// GET /ping
+	if _, body := testRequest(t, ts, "GET", "/ping", nil); body != "." {
+		t.Fatalf(body)
+	}
+
+	// GET /pingall
+	if _, body := testRequest(t, ts, "GET", "/pingall", nil); body != "ping all" {
+		t.Fatalf(body)
+	}
+
+	// GET /ping/all
+	if _, body := testRequest(t, ts, "GET", "/ping/all", nil); body != "ping all" {
+		t.Fatalf(body)
+	}
+
+	// GET /ping/all2
+	if _, body := testRequest(t, ts, "GET", "/ping/all2", nil); body != "ping all2" {
+		t.Fatalf(body)
+	}
+
+	// GET /ping/123
+	if _, body := testRequest(t, ts, "GET", "/ping/123", nil); body != "ping one id: 123" {
+		t.Fatalf(body)
+	}
+
+	// GET /ping/allan
+	if _, body := testRequest(t, ts, "GET", "/ping/allan", nil); body != "ping one id: allan" {
+		t.Fatalf(body)
+	}
+
+	// GET /ping/1/woop
+	if _, body := testRequest(t, ts, "GET", "/ping/1/woop", nil); body != "woop.1" {
+		t.Fatalf(body)
+	}
+
+	// HEAD /ping
+	resp, err := http.Head(ts.URL + "/ping")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("head failed, should be 200")
+	}
+	if resp.Header.Get("X-Ping") == "" {
+		t.Error("expecting X-Ping header")
+	}
+
+	// GET /admin/catch-this
+	if _, body := testRequest(t, ts, "GET", "/admin/catch-thazzzzz", nil); body != "catchall" {
+		t.Fatalf(body)
+	}
+
+	// POST /admin/catch-this
+	resp, err = http.Post(ts.URL+"/admin/casdfsadfs", "text/plain", bytes.NewReader([]byte{}))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		t.Error("POST failed, should be 200")
+	}
+
+	if string(body) != "catchall" {
+		t.Error("expecting response body: 'catchall'")
+	}
+
+	// Custom http method DIE /ping/1/woop
+	if resp, body := testRequest(t, ts, "DIE", "/ping/1/woop", nil); body != "" || resp.StatusCode != 405 {
+		t.Fatalf(fmt.Sprintf("expecting 405 status and empty body, got %d '%s'", resp.StatusCode, body))
+	}
+}
+
+func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io.Reader) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, body)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+	defer resp.Body.Close()
+
+	return resp, string(respBody)
+}
